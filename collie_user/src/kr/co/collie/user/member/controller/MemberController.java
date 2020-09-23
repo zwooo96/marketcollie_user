f2a6b33f36586144639a1d9ffc3db3c531939d88
phg <ehdcjf753@naver.com>
2020-09-23 17:41:11 +0900

    dd

--- collie_user/src/kr/co/collie/user/member/controller/MemberController.java
+++ collie_user/src/kr/co/collie/user/member/controller/MemberController.java
@@ -2,8 +2,13 @@ package kr.co.collie.user.member.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import kr.co.collie.user.member.vo.JoinVO;
+
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+import javax.servlet.http.HttpServletRequest;
 @Controller
 public class MemberController {
 	
@@ -12,6 +17,13 @@ public class MemberController {
 		
 		
 		return "";
-	}
+	}//
+	
+	@RequestMapping(value="/join_process.do", method= GET)
+	public String join(JoinVO jVO, HttpServletRequest request) {
+		
+		
+		return "";
+	}//join
 	
 }//class
--- collie_user/src/kr/co/collie/user/member/vo/JoinVO.java
+++ collie_user/src/kr/co/collie/user/member/vo/JoinVO.java
@@ -1,5 +1,79 @@
 package kr.co.collie.user.member.vo;
 
 public class JoinVO {
+	String id, pass, name, email, phone, addr, addrDetail, zipCode,ip;
 
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getAddr() {
+		return addr;
+	}
+
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+
+	public String getAddrDetail() {
+		return addrDetail;
+	}
+
+	public void setAddrDetail(String addrDetail) {
+		this.addrDetail = addrDetail;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	public String getIp() {
+		return ip;
+	}
+
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+	
+	
 }
