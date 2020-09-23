8620de766e1c0d52fbcda98395dc7092e14e07c6
Yuha Lee <yuha.lee.dev@gmail.com>
2020-09-23 17:38:49 +0900

    webContent에 member폴더 추가

--- collie_user/src/kr/co/collie/user/member/domain/LoginDomain.java
+++ collie_user/src/kr/co/collie/user/member/domain/LoginDomain.java
@@ -1,5 +1,57 @@
 package kr.co.collie.user.member.domain;
 
 public class LoginDomain {
+	
+	private int memberNum;
+	
+	public int getMemberNum() {
+		return memberNum;
+	}
+	public void setMemberNum(int memberNum) {
+		this.memberNum = memberNum;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getAddr() {
+		return addr;
+	}
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+	public String getAddrDetail() {
+		return addrDetail;
+	}
+	public void setAddrDetail(String addrDetail) {
+		this.addrDetail = addrDetail;
+	}
+	public String getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+	private String id, name, email, phone, addr, addrDetail, zipCode;
 
 }
