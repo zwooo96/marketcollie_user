b4caabf555110ef2d6afbfe1c61075caaa259768
zwooo <dlwldn385@naver.com>
2020-09-23 20:22:30 +0900

    장바구니 담기

--- collie_user/src/kr/co/collie/user/cart/dao/CartDAO.java
+++ collie_user/src/kr/co/collie/user/cart/dao/CartDAO.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apache.ibatis.session.SqlSession;
 
 import kr.co.collie.user.cart.domain.CartGoodsDomain;
+import kr.co.collie.user.cart.vo.CartVO;
 import kr.co.collie.user.dao.GetCollieHandler;
 
 public class CartDAO {
@@ -20,6 +21,21 @@ public class CartDAO {
 		}//end if
 		return cDAO;
 	}//getInstance
+	
+	/**
+	 * 장바구니에 상품을 추가하는 일
+	 * @param cVO
+	 * @return
+	 */
+	public int insertCart(CartVO cVO) {
+		int cnt = 0;
+		
+		SqlSession ss = GetCollieHandler.getInstance().getSqlSession();
+		cnt = ss.insert("insertCart", cVO);
+		ss.close();
+		
+		return cnt;
+	}//insertCart
 	
 	public List<CartGoodsDomain> selectCartGoods(int memberNum){
 		List<CartGoodsDomain> list=null;
