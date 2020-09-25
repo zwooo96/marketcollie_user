c96dad7dbfc715926bb27aae7bb7b4ffea7952a3
knewbean <knewbean@daum.net>
2020-09-25 17:02:59 +0900

    local commit

--- collie_user/src/kr/co/collie/user/item/controller/ItemController.java
+++ collie_user/src/kr/co/collie/user/item/controller/ItemController.java
@@ -1,5 +1,23 @@
 package kr.co.collie.user.item.controller;
 
-public class ItemController {
+import java.util.List;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import kr.co.collie.user.item.domain.ItemListDomain;
+import kr.co.collie.user.item.service.ItemService;
+
+@Controller
+public class ItemController {
+	
+	@GetMapping(value="/item/search.do")
+	public String getSearchList(String keyword, Model model) {
+		
+		ItemService iService = new ItemService();
+		List<ItemListDomain> list = iService.getSearchList(keyword);
+		model.addAttribute("search_result", list);
+		return "item/searchItem_result";
+	}
 }
