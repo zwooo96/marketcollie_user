169315b6611c66a4f6a43e619bb49a788c225ba6
zwooo <dlwldn385@naver.com>
2020-09-26 14:37:48 +0900

    Mypage 비밀번호 확인

--- collie_user/src/kr/co/collie/user/mypage/service/MypageService.java
+++ collie_user/src/kr/co/collie/user/mypage/service/MypageService.java
@@ -1,5 +1,31 @@
 package kr.co.collie.user.mypage.service;
 
-public class MypageService {
+import kr.co.collie.user.mypage.dao.MypageDAO;
+import kr.co.collie.user.mypage.vo.PassCheckVO;
+import kr.co.collie.user.mypage.vo.UpdatePassVO;
 
+public class MypageService {
+	
+	public boolean getMemberPass(PassCheckVO pcVO) {
+		boolean flag = false;
+		
+		MypageDAO mpDAO = MypageDAO.getInstance();
+		flag = mpDAO.selectMemberPass(pcVO) != 0;
+		
+		return flag;
+	}//getMemberPass
+	
+	/**
+	 * 비밀번호를 변경하는 일
+	 * @param upVO
+	 * @return
+	 */
+	public boolean modifyPass(UpdatePassVO upVO) {
+		boolean flag = false;
+		
+		MypageDAO mpDAO = MypageDAO.getInstance();
+		flag = mpDAO.updateMemberPass(upVO)==1;
+		
+		return flag;
+	}//modifyPass
 }
