c26b85a9d465e62c9e9b51051a3fd6816865fa57
phg <ehdcjf753@naver.com>
2020-09-28 15:28:52 +0900

    dd

--- collie_user/src/kr/co/collie/user/mypage/vo/ModifyMemberVO.java
+++ collie_user/src/kr/co/collie/user/mypage/vo/ModifyMemberVO.java
@@ -1,5 +1,46 @@
 package kr.co.collie.user.mypage.vo;
 
 public class ModifyMemberVO {
+	private String name, phone, addr, addr_detail, zipCode;
 
-}
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getAddr() {
+		return addr;
+	}
+
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+
+	public String getAddr_detail() {
+		return addr_detail;
+	}
+
+	public void setAddr_detail(String addr_detail) {
+		this.addr_detail = addr_detail;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+	
+}//ModifyMemberVO
